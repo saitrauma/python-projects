c3b7c6394ef64d39d4545ad09983d5f12ef62ac8
Saiganesh <saiganeshsaga@gmail.com>
2025-11-04 00:16:32 +0530

    Add files via upload

---
+++ project day 1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("welcome to my first python project")
+city_born = input("where were u born? ")
+pet_name = input("what is the name of your pet?")
+print("your band name is: " + city_born + " " + pet_name)
